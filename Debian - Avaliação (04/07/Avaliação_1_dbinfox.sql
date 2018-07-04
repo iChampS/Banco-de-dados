fd9415b107d988803d3d839d0c966c7298be30a9
iChampS <38111117+iChampS@users.noreply.github.com>
2018-07-04 16:56:29 -0300

    Avaliação - Script (04/07/2018)

---
+++ Debian - Avaliação (04/07/Avaliação_1_dbinfox.sql
@@ -0,0 +1,22 @@
+use dbinfox;
+-- inspecionar tabelas no banco de dados
+show tables;
+-- o código abaixo cria uma tabela
+create table tb_usuarios(iduser int primary key,
+usuario varchar(50) not null,
+login varchar(50) not null unique,
+senha varchar(50) not null,
+fone varchar(15),
+perfil varchar(50) not null);
+show tables;
+-- descrevendo a tabela
+describe tb_usuarios;
+-- inserindo dados na tabela (Create)
+insert into tb_usuarios values
+(1,'Victor Guilherme Leite de Medeiros','victorlm1','victor123','91111-1111','Aluno do Senac');
+insert into tb_usuarios values
+(2,'Bruno Rodrigues','brunor2','bruno123','92222-2222','Aluno do Senac');
+insert into tb_usuarios values
+(3,'Lucas Souza','lucass3','lucas123','93333-3333','Aluno do Senac');
+-- pesquisando dados na tabela (Read)
+select * from tb_usuarios;
